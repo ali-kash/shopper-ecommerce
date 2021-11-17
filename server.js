3147c1583d81dfd0e48e13c4fac152bdd6e14b20
Kai <ali.rf.kashani@gmail.com>
2021-11-17 13:22:30 -0500

    creating signup form, added server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+// import packages
+const express = require('express')
+const admin = require('firebase-admin')
+const bcrypt = require('bcrypt')
+const path = require('path')
+
+// declare static path
+let staticPath = path.join(__dirname, 'public')
+
+// initialize express.js
+const app = express()
+
+// middlewares
+app.use(express.static(staticPath))
+
+// routes
+// --- home route
+app.get('/', (req, res) => {
+	res.sendFile(path.join(staticPath, 'index.html'))
+})
+
+// --- product route
+app.get('/product', (req, res) => {
+	res.sendFile(path.join(staticPath, 'product.html'))
+})
+
+// --- search route
+app.get('/search', (req, res) => {
+	res.sendFile(path.join(staticPath, 'search.html'))
+})
+
+// --- signup route
+app.get('/signup', (req, res) => {
+	res.sendFile(path.join(staticPath, 'signup.html'))
+})
+
+// --- 404 route
+app.get('/404', (req, res) => {
+	res.sendFile(path.join(staticPath, '404.html'))
+})
+
+app.use((req, res) => {
+	res.redirect('/404')
+})
+
+app.listen(3000, () => {
+	console.log('listening on Port 3000...')
+})

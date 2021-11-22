ad8fc9e8cffcc9a95990e33c7920e2d1dac9ff78
Kai <ali.rf.kashani@gmail.com>
2021-11-22 15:08:56 -0500

    updates

--- public/js/product.js
+++ public/js/product.js
@@ -22,5 +22,39 @@ sizeBtns.forEach((item, i) => {
 		sizeBtns[checkedBtn].classList.remove('check')
 		item.classList.add('check')
 		checkedBtn = i
+		console.log(item)
 	})
 })
+
+// const data = [
+// 	{
+// 		name: 'Rehan',
+// 		location: 'Pune',
+// 		description: 'hello hi',
+// 		created_by: 13692,
+// 		users_name: 'xyz',
+// 	},
+// 	{
+// 		name: 'Sameer',
+// 		location: 'Bangalore',
+// 		description: 'how are you',
+// 		created_by: 13543,
+// 		users_name: 'abc',
+// 	},
+// ]
+
+// var htmlText = data.map(function (o) {
+// 	return `
+//       <div class="div-conatiner">
+//       <p class="p-name"> Name: ${o.name}</p>
+//       <p class="p-loc"> Location: ${o.location}</p>
+//       <p class="p-desc"> Description: ${o.description}</p>
+//       <p class="p-created"> Created by: ${o.created_by}</p>
+//      <p class="p-uname"> Username: ${o.users_name}</p>
+//      </div>
+//   `
+// })
+
+// const mainContainer = document.querySelector('#myData')
+
+// mainContainer.innerHtml = htmlText
